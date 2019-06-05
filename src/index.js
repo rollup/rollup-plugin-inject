43d44c59100f24205cc3aeb283174561b5fbad17
Denis Bardadym <bardadymchik@gmail.com>
2019-06-05 13:16:31 +0300

    Modernize code

--- src/index.js
+++ src/index.js
@@ -1,199 +1,190 @@
-import {
-	attachScopes,
-	createFilter,
-	makeLegalIdentifier
-} from 'rollup-pluginutils';
-import { sep } from 'path';
-import { walk } from 'estree-walker';
+import { attachScopes, createFilter, makeLegalIdentifier } from "rollup-pluginutils";
+import { sep } from "path";
+import { walk } from "estree-walker";
 
-import MagicString from 'magic-string';
+import MagicString from "magic-string";
 
-function escape (str) {
-	return str.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&');
+function escape(str) {
+  return str.replace(/[-[\]/{}()*+?.\\^$|]/g, "\\$&");
 }
 
-function isReference (node, parent) {
-	if (node.type === 'MemberExpression') {
-		return !node.computed && isReference(node.object, node);
-	}
+function isReference(node, parent) {
+  if (node.type === "MemberExpression") {
+    return !node.computed && isReference(node.object, node);
+  }
 
-	if (node.type === 'Identifier') {
-		// TODO is this right?
-		if (parent.type === 'MemberExpression')
-			return parent.computed || node === parent.object;
+  if (node.type === "Identifier") {
+    // TODO is this right?
+    if (parent.type === "MemberExpression") return parent.computed || node === parent.object;
 
-		// disregard the `bar` in { bar: foo }
-		if (parent.type === 'Property' && node !== parent.value) return false;
+    // disregard the `bar` in { bar: foo }
+    if (parent.type === "Property" && node !== parent.value) return false;
 
-		// disregard the `bar` in `class Foo { bar () {...} }`
-		if (parent.type === 'MethodDefinition') return false;
+    // disregard the `bar` in `class Foo { bar () {...} }`
+    if (parent.type === "MethodDefinition") return false;
 
-		// disregard the `bar` in `export { foo as bar }`
-		if (parent.type === 'ExportSpecifier' && node !== parent.local) return;
+    // disregard the `bar` in `export { foo as bar }`
+    if (parent.type === "ExportSpecifier" && node !== parent.local) return;
 
-		return true;
-	}
+    return true;
+  }
 }
 
-function flatten (node) {
-	let name;
-	let parts = [];
+function flatten(node) {
+  const parts = [];
 
-	while (node.type === 'MemberExpression') {
-		parts.unshift(node.property.name);
-		node = node.object;
-	}
+  while (node.type === "MemberExpression") {
+    parts.unshift(node.property.name);
+    node = node.object;
+  }
 
-	name = node.name;
-	parts.unshift(name);
+  const name = node.name;
+  parts.unshift(name);
 
-	return { name, keypath: parts.join('.') };
+  return { name, keypath: parts.join(".") };
 }
 
-export default function inject (options) {
-	if (!options) throw new Error('Missing options');
+export default function inject(options) {
+  if (!options) throw new Error("Missing options");
 
-	const filter = createFilter(options.include, options.exclude);
+  const filter = createFilter(options.include, options.exclude);
 
-	let modules;
+  let modules;
 
-	if (options.modules) {
-		modules = options.modules;
-	} else {
-		modules = Object.assign({}, options);
-		delete modules.include;
-		delete modules.exclude;
-	}
+  if (options.modules) {
+    modules = options.modules;
+  } else {
+    modules = Object.assign({}, options);
+    delete modules.include;
+    delete modules.exclude;
+  }
 
-	// Fix paths on Windows
-	if (sep !== '/') {
-		Object.keys(modules).forEach(key => {
-			const module = modules[key];
+  // Fix paths on Windows
+  if (sep !== "/") {
+    Object.keys(modules).forEach(key => {
+      const module = modules[key];
 
-			modules[key] = Array.isArray(module)
-				? [module[0].split(sep).join('/'), module[1]]
-				: module.split(sep).join('/');
-		});
-	}
+      modules[key] = Array.isArray(module)
+        ? [module[0].split(sep).join("/"), module[1]]
+        : module.split(sep).join("/");
+    });
+  }
 
-	const firstpass = new RegExp(
-		`(?:${Object.keys(modules)
-			.map(escape)
-			.join('|')})`,
-		'g'
-	);
-	const sourceMap = options.sourceMap !== false;
+  const firstpass = new RegExp(
+    `(?:${Object.keys(modules)
+      .map(escape)
+      .join("|")})`,
+    "g"
+  );
+  const sourceMap = options.sourceMap !== false;
 
-	return {
-		name: 'inject',
+  return {
+    name: "inject",
 
-		transform (code, id) {
-			if (!filter(id)) return null;
-			if (code.search(firstpass) == -1) return null;
+    transform(code, id) {
+      if (!filter(id)) return null;
+      if (code.search(firstpass) == -1) return null;
 
-			if (sep !== '/') id = id.split(sep).join('/');
+      if (sep !== "/") id = id.split(sep).join("/");
 
-			let ast = null;
-			try {
-				ast = this.parse(code);
-			} catch (err) {
-				this.warn({
-					code: 'PARSE_ERROR',
-					message:
-						`rollup-plugin-inject: failed to parse ${id}. Consider restricting the plugin to particular files via options.include`
-				});
-			}
-			if (!ast) return null;
+      let ast = null;
+      try {
+        ast = this.parse(code);
+      } catch (err) {
+        this.warn({
+          code: "PARSE_ERROR",
+          message: `rollup-plugin-inject: failed to parse ${id}. Consider restricting the plugin to particular files via options.include`
+        });
+      }
+      if (!ast) return null;
 
-			// analyse scopes
-			let scope = attachScopes(ast, 'scope');
+      // analyse scopes
+      let scope = attachScopes(ast, "scope");
 
-			let imports = {};
-			ast.body.forEach(node => {
-				if (node.type === 'ImportDeclaration') {
-					node.specifiers.forEach(specifier => {
-						imports[specifier.local.name] = true;
-					});
-				}
-			});
+      const imports = {};
+      ast.body.forEach(node => {
+        if (node.type === "ImportDeclaration") {
+          node.specifiers.forEach(specifier => {
+            imports[specifier.local.name] = true;
+          });
+        }
+      });
 
-			const magicString = new MagicString(code);
+      const magicString = new MagicString(code);
 
-			let newImports = {};
+      const newImports = {};
 
-			function handleReference (node, name, keypath) {
-				if (keypath in modules && !scope.contains(name) && !imports[name]) {
-					let module = modules[keypath];
-					if (typeof module === 'string') module = [module, 'default'];
+      function handleReference(node, name, keypath) {
+        if (keypath in modules && !scope.contains(name) && !imports[name]) {
+          let module = modules[keypath];
+          if (typeof module === "string") module = [module, "default"];
 
-					// prevent module from importing itself
-					if (module[0] === id) return;
+          // prevent module from importing itself
+          if (module[0] === id) return;
 
-					const hash = `${keypath}:${module[0]}:${module[1]}`;
+          const hash = `${keypath}:${module[0]}:${module[1]}`;
 
-					const importLocalName =
-						name === keypath ? name : makeLegalIdentifier(`$inject_${keypath}`);
+          const importLocalName =
+            name === keypath ? name : makeLegalIdentifier(`$inject_${keypath}`);
 
-					if (!newImports[hash]) {
-						if (module[1] === '*') {
-							newImports[hash] = `import * as ${importLocalName} from '${
-								module[0]
-							}';`;
-						} else {
-							newImports[hash] = `import { ${
-								module[1]
-							} as ${importLocalName} } from '${module[0]}';`;
-						}
-					}
+          if (!newImports[hash]) {
+            if (module[1] === "*") {
+              newImports[hash] = `import * as ${importLocalName} from '${module[0]}';`;
+            } else {
+              newImports[hash] = `import { ${module[1]} as ${importLocalName} } from '${
+                module[0]
+              }';`;
+            }
+          }
 
-					if (name !== keypath) {
-						magicString.overwrite(node.start, node.end, importLocalName, {
-							storeName: true
-						});
-					}
+          if (name !== keypath) {
+            magicString.overwrite(node.start, node.end, importLocalName, {
+              storeName: true
+            });
+          }
 
-					return true;
-				}
-			}
+          return true;
+        }
+      }
 
-			walk(ast, {
-				enter (node, parent) {
-					if (sourceMap) {
-						magicString.addSourcemapLocation(node.start);
-						magicString.addSourcemapLocation(node.end);
-					}
+      walk(ast, {
+        enter(node, parent) {
+          if (sourceMap) {
+            magicString.addSourcemapLocation(node.start);
+            magicString.addSourcemapLocation(node.end);
+          }
 
-					if (node.scope) scope = node.scope;
+          if (node.scope) scope = node.scope;
 
-					// special case – shorthand properties. because node.key === node.value,
-					// we can't differentiate once we've descended into the node
-					if (node.type === 'Property' && node.shorthand) {
-						const name = node.key.name;
-						handleReference(node, name, name);
-						return this.skip();
-					}
+          // special case – shorthand properties. because node.key === node.value,
+          // we can't differentiate once we've descended into the node
+          if (node.type === "Property" && node.shorthand) {
+            const name = node.key.name;
+            handleReference(node, name, name);
+            return this.skip();
+          }
 
-					if (isReference(node, parent)) {
-						const { name, keypath } = flatten(node);
-						const handled = handleReference(node, name, keypath);
-						if (handled) return this.skip();
-					}
-				},
-				leave (node) {
-					if (node.scope) scope = scope.parent;
-				}
-			});
+          if (isReference(node, parent)) {
+            const { name, keypath } = flatten(node);
+            const handled = handleReference(node, name, keypath);
+            if (handled) return this.skip();
+          }
+        },
+        leave(node) {
+          if (node.scope) scope = scope.parent;
+        }
+      });
 
-			const keys = Object.keys(newImports);
-			if (!keys.length) return null;
+      const keys = Object.keys(newImports);
+      if (!keys.length) return null;
 
-			const importBlock = keys.map(hash => newImports[hash]).join('\n\n');
-			magicString.prepend(importBlock + '\n\n');
+      const importBlock = keys.map(hash => newImports[hash]).join("\n\n");
+      magicString.prepend(importBlock + "\n\n");
 
-			return {
-				code: magicString.toString(),
-				map: sourceMap ? magicString.generateMap() : null
-			};
-		}
-	};
+      return {
+        code: magicString.toString(),
+        map: sourceMap ? magicString.generateMap() : null
+      };
+    }
+  };
 }
--- test/test.js
+++ test/test.js
@@ -1,222 +1,163 @@
-var assert = require('assert');
-var path = require('path');
-var rollup = require('rollup');
-var inject = require('..');
+const assert = require("assert");
+const path = require("path");
+const { rollup } = require("rollup");
+const inject = require("..");
 
 process.chdir(__dirname);
 
-describe('rollup-plugin-inject', function () {
-	it('inserts a default import statement', function () {
-		return rollup
-			.rollup({
-				input: 'samples/basic/main.js',
-				plugins: [inject({ $: 'jquery' })],
-				external: ['jquery']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+describe("rollup-plugin-inject", () => {
+  it("inserts a default import statement", async () => {
+    const bundle = await rollup({
+      input: "samples/basic/main.js",
+      plugins: [inject({ $: "jquery" })],
+      external: ["jquery"]
+    });
 
-				assert.ok(
-					code.indexOf('import $ from \'jquery\'') !== -1,
-					generated.code
-				);
-			});
-	});
+    const { output } = await bundle.generate({ format: "es" });
 
-	it('uses the modules property', function () {
-		return rollup
-			.rollup({
-				input: 'samples/basic/main.js',
-				plugins: [
-					inject({
-						modules: { $: 'jquery' }
-					})
-				],
-				external: ['jquery']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+    const { code } = output[0];
 
-				assert.ok(
-					code.indexOf('import $ from \'jquery\'') !== -1,
-					generated.code
-				);
-			});
-	});
+    assert.ok(code.indexOf("import $ from 'jquery'") !== -1, code);
+  });
 
-	it('inserts a named import statement', function () {
-		return rollup
-			.rollup({
-				input: 'samples/named/main.js',
-				plugins: [inject({ Promise: ['es6-promise', 'Promise'] })],
-				external: ['es6-promise']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+  it("uses the modules property", async () => {
+    const bundle = await rollup({
+      input: "samples/basic/main.js",
+      plugins: [
+        inject({
+          modules: { $: "jquery" }
+        })
+      ],
+      external: ["jquery"]
+    });
 
-				assert.ok(
-					code.indexOf('import { Promise } from \'es6-promise\'') !== -1,
-					generated.code
-				);
-			});
-	});
+    const { output } = await bundle.generate({ format: "es" });
+    const { code } = output[0];
 
-	it('overwrites keypaths', function () {
-		return rollup
-			.rollup({
-				input: 'samples/keypaths/main.js',
-				plugins: [
-					inject({
-						'Object.assign': path.resolve(
-							'samples/keypaths/polyfills/object-assign.js'
-						)
-					})
-				]
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+    assert.ok(code.indexOf("import $ from 'jquery'") !== -1, code);
+  });
 
-				assert.notEqual(
-					code.indexOf('var clone = $inject_Object_assign'),
-					-1,
-					code
-				);
-				assert.notEqual(code.indexOf('var $inject_Object_assign ='), -1, code);
-			});
-	});
+  it("inserts a named import statement", async () => {
+    const bundle = await rollup({
+      input: "samples/named/main.js",
+      plugins: [inject({ Promise: ["es6-promise", "Promise"] })],
+      external: ["es6-promise"]
+    });
 
-	it('ignores existing imports', function () {
-		return rollup
-			.rollup({
-				input: 'samples/existing/main.js',
-				plugins: [inject({ $: 'jquery' })],
-				external: ['jquery']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+    const { output } = await bundle.generate({ format: "es" });
+    const { code } = output[0];
 
-				code = code.replace(/import \$.+/, ''); // remove first instance. there shouldn't be a second
+    assert.ok(code.indexOf("import { Promise } from 'es6-promise'") !== -1, code);
+  });
 
-				assert.ok(
-					code.indexOf('import $ from \'jquery\'') === -1,
-					generated.code
-				);
-			});
-	});
+  it("overwrites keypaths", async () => {
+    const bundle = await rollup({
+      input: "samples/keypaths/main.js",
+      plugins: [
+        inject({
+          "Object.assign": path.resolve("samples/keypaths/polyfills/object-assign.js")
+        })
+      ]
+    });
+    const { output } = await bundle.generate({ format: "es" });
+    const { code } = output[0];
 
-	it('handles shadowed variables', function () {
-		return rollup
-			.rollup({
-				input: 'samples/shadowing/main.js',
-				plugins: [inject({ $: 'jquery' })],
-				external: ['jquery']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+    assert.notEqual(code.indexOf("var clone = $inject_Object_assign"), -1, code);
+    assert.notEqual(code.indexOf("var $inject_Object_assign ="), -1, code);
+  });
 
-				assert.ok(code.indexOf('\'jquery\'') === -1, generated.code);
-			});
-	});
+  it("ignores existing imports", async () => {
+    const bundle = await rollup({
+      input: "samples/existing/main.js",
+      plugins: [inject({ $: "jquery" })],
+      external: ["jquery"]
+    });
+    const { output } = await bundle.generate({ format: "es" });
+    let { code } = output[0];
 
-	it('handles shorthand properties', function () {
-		return rollup
-			.rollup({
-				input: 'samples/shorthand/main.js',
-				plugins: [inject({ Promise: ['es6-promise', 'Promise'] })],
-				external: ['es6-promise']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+    code = code.replace(/import \$.+/, ""); // remove first instance. there shouldn't be a second
 
-				assert.ok(
-					code.indexOf('import { Promise } from \'es6-promise\'') !== -1,
-					generated.code
-				);
-			});
-	});
+    assert.ok(code.indexOf("import $ from 'jquery'") === -1, output[0].code);
+  });
 
-	it('handles redundant keys', function () {
-		return rollup
-			.rollup({
-				input: 'samples/redundant-keys/main.js',
-				plugins: [
-					inject({
-						Buffer: 'Buffer',
-						'Buffer.isBuffer': 'is-buffer'
-					})
-				],
-				external: ['Buffer', 'is-buffer']
-			})
-			.then(function (bundle) {
-				assert.deepEqual(bundle.imports, ['is-buffer']);
-			});
-	});
+  it("handles shadowed variables", async () => {
+    const bundle = await rollup({
+      input: "samples/shadowing/main.js",
+      plugins: [inject({ $: "jquery" })],
+      external: ["jquery"]
+    });
+    const { output } = await bundle.generate({ format: "es" });
 
-	it('generates * imports', function () {
-		return rollup
-			.rollup({
-				input: 'samples/import-namespace/main.js',
-				plugins: [inject({ foo: ['foo', '*'] })],
-				external: ['foo']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'es' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+    const { code } = output[0];
 
-				assert.ok(
-					code.indexOf('import { bar, baz } from \'foo\'') !== -1,
-					generated.code
-				);
-			});
-	});
+    assert.ok(code.indexOf("'jquery'") === -1, code);
+  });
 
-	it('transpiles non-JS files but handles failures to parse', function () {
-		return rollup
-			.rollup({
-				input: 'samples/non-js/main.js',
-				plugins: [
-					inject({ relative: ['path', 'relative'] }),
-					{
-						transform (code, id) {
-							if (/css/.test(id)) {
-								return '';
-							}
-						}
-					}
-				],
-				external: ['path']
-			})
-			.then(function (bundle) {
-				return bundle.generate({ format: 'cjs' });
-			})
-			.then(function (generated) {
-				var code = generated.code;
+  it("handles shorthand properties", async () => {
+    const bundle = await rollup({
+      input: "samples/shorthand/main.js",
+      plugins: [inject({ Promise: ["es6-promise", "Promise"] })],
+      external: ["es6-promise"]
+    });
+    const { output } = await bundle.generate({ format: "es" });
 
-				var fn = new Function('require', 'assert', code);
-				fn(require, assert);
-			});
-	});
+    const { code } = output[0];
+
+    assert.ok(code.indexOf("import { Promise } from 'es6-promise'") !== -1, code);
+  });
+
+  it("handles redundant keys", async () => {
+    const bundle = await rollup({
+      input: "samples/redundant-keys/main.js",
+      plugins: [
+        inject({
+          Buffer: "Buffer",
+          "Buffer.isBuffer": "is-buffer"
+        })
+      ],
+      external: ["Buffer", "is-buffer"]
+    });
+
+    const { output } = await bundle.generate({ format: "es" });
+
+    const { imports } = output[0];
+
+    assert.deepEqual(imports, ["is-buffer"]);
+  });
+
+  it("generates * imports", async () => {
+    const bundle = await rollup({
+      input: "samples/import-namespace/main.js",
+      plugins: [inject({ foo: ["foo", "*"] })],
+      external: ["foo"]
+    });
+    const { output } = await bundle.generate({ format: "es" });
+
+    const { code } = output[0];
+
+    assert.ok(code.indexOf("import { bar, baz } from 'foo'") !== -1, code);
+  });
+
+  it("transpiles non-JS files but handles failures to parse", async () => {
+    const bundle = await rollup({
+      input: "samples/non-js/main.js",
+      plugins: [
+        inject({ relative: ["path", "relative"] }),
+        {
+          transform(code, id) {
+            if (/css/.test(id)) {
+              return "";
+            }
+          }
+        }
+      ],
+      external: ["path"]
+    });
+    const { output } = await bundle.generate({ format: "cjs" });
+
+    const { code } = output[0];
+
+    const fn = new Function("require", "assert", code);
+    fn(require, assert);
+  });
 });
